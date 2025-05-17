f10313999b87a2134dffbe11baa630c46feb9980
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2025-05-17 18:55:45 +0100

    Routing

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const members = [
+  {
+    id: 1,
+    name: "Aniagolu Chiemelie",
+    email: "aniagoluchiemelie77@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Munachi Aniagolu",
+    email: "munachianiagolu01@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Diamaka Aniagolu",
+    email: "diamakaaniagolu99@gmail.com",
+    status: "inactive",
+  },
+];
+//Gets All Members
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
 //Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 5000;
